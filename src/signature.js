4416f9e4732a54d93776410b35ee0ced0cc02705
Denis Golubovskiy <bukashk0zzz@me.com>
2019-05-08 13:23:39 +0300

    Fix cache

--- src/signature.js
+++ src/signature.js
@@ -1,8 +1,9 @@
 module.exports = function(secret) {
   const crypto = require('crypto');
-  const hash = crypto.createHmac('sha256', secret);
 
   return function(signature, width, height, key) {
+    let hash = crypto.createHmac('sha256', secret);
+
     return signature === hash.update(width + 'x' + height + '/' + key).digest('hex')
   }
 };
